4a050fe37cd8e19db6f8febe51398379a16321d3
Goldfor <dolg.almaz@yandex.ru>
2023-10-17 17:49:38 +0300

    v 0.0.10
    Исправление прошивки по bluetooth

--- src/IS_Bluetooth.cpp
+++ src/IS_Bluetooth.cpp
@@ -3,7 +3,21 @@
 void enter_bootloader(void)
 {
 
+  char buf[32];
+  uint16_t ind = 0;
+  sprintf((char*)buf,"Bootloader Mode\n\r");
+  while (ind < 17)
+  {
+    while (READ_BIT(USART1->SR, USART_SR_TXE) != (USART_SR_TXE)) {}
+    USART1->DR = buf[ind];
+    ind++;
+  }
+
+
   HAL_Delay(200);
+
+
+  CLEAR_BIT(USART1->CR1, (USART_CR1_UE));
 
   void (*SysMemBootJump)(void);
   volatile uint32_t addr = 0x1FFFF000;
